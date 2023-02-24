9fcd1b1a67fc24456e45744e2451d171defb9572
ivanramirez_git <ivanrene10@gmail.com>
2023-02-24 05:13:28 -0500

    Packages and Dependences

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ivanramirez-git/DoublePartners/my_ticket_api_golang/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
+
 }
